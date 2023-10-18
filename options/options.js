16e976c283f87c3995748662314160524d47a665
Crowdbotics <team@crowdbotics.com>
2023-10-18 20:11:56 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "samsapp-44022",
+  url: "https://samsapp-44022.botics.co",
+  api: "https://samsapp-44022.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
